70babc6ed75c86e9d169d2f977263cebead182f4
Ángel González <angotov@gmail.com>
2021-02-05 04:07:28 +0100

    Figures now follow the player and can also be picked up along the way.

--- GranCITM_Game_Jam/Women_In_STEAM/Assets/Scripts/FigureController.cs
+++ GranCITM_Game_Jam/Women_In_STEAM/Assets/Scripts/FigureController.cs
@@ -23,19 +23,33 @@ public class FigureController : MonoBehaviour
 
     public LayerMask            m_ground_layer;
     public LayerMask            m_hazards_layer;
+    public LayerMask            m_player_layer;
 
     public Transform            m_feet;
-    public float                ground_collision_radius     = 0.1f;
-    public float                player_collision_radius     = 0.5f;
+    public float                ground_collision_radius             = 0.1f;
+    public float                player_collision_radius             = 0.5f;
 
-    public float                m_jump_force                = 350.0f;
-    public float                m_jump_time                 = 0.4f;
+    public float                m_jump_force                        = 350.0f;
+    public float                m_jump_time                         = 0.4f;
 
     private Rigidbody2D         rb;
     private CapsuleCollider2D   col;
-    private Vector2             jump_velocity               = Vector2.zero;
-    private float               jump_time_counter           = 0.0f;
-    private bool                jump_released               = false;
+    private Vector2             jump_velocity                       = Vector2.zero;
+    private float               jump_time_counter                   = 0.0f;
+    private bool                jump_released                       = false;
+
+    // --- PLAYER VARIABLES ---
+    private Transform           player_transform;
+    private PlayerController    player_controller;
+
+    // --- JUICE VARIABLES ---
+    private float               position_behind_player              = 0.0f;
+    private float               new_position_behind_player          = 0.0f;
+    private float               original_position_behind_player     = 0.0f;
+    private bool                calculated_new_pos                  = false;
+    private float               total_lerp_time                     = 2.0f;
+    private float               current_lerp_time                   = 0.0f;
+
 
     // Start is called before the first frame update
     void Start()
@@ -48,7 +62,7 @@ public class FigureController : MonoBehaviour
     {
         UpdateFigureState();
 
-        if (FigureIsActive())
+        if (figure_state == FIGURE_STATE.ACTIVE)
         {
             UpdateJumpingState();
         }
@@ -62,25 +76,69 @@ public class FigureController : MonoBehaviour
             // DESTROY THIS INSTACE?
         }
 
-        if (CheckForPlayerPickUp())
+        switch (figure_state)
         {
-            figure_state = FIGURE_STATE.ACTIVE;
+            case FIGURE_STATE.IDLE:     { CheckForPlayerPickUp(); } break;
+            case FIGURE_STATE.ACTIVE:   { FollowPlayer(); }         break;
+            case FIGURE_STATE.DEAD:     { DestroyFigure(); }        break;
         }
     }
 
     bool CheckForFigureDeath()
     {
-        return false;
+        bool ret = false;
+
+
+
+        return ret;
     }
 
     bool CheckForPlayerPickUp()
     {
-        return false;
+        bool ret = false;
+
+        if (Physics2D.OverlapCircle(transform.position, player_collision_radius, m_player_layer))
+        {
+            figure_state        = FIGURE_STATE.ACTIVE;
+            transform.position  = new Vector3(player_transform.position.x - position_behind_player, player_transform.position.y, player_transform.position.z);
+
+            player_controller.AddFigure();
+        } 
+
+        return ret;
     }
 
-    bool FigureIsActive()
+    void FollowPlayer()
     {
-        return (figure_state == FIGURE_STATE.ACTIVE && figure_state != FIGURE_STATE.DEAD);
+        transform.position = new Vector3(player_transform.position.x - position_behind_player, transform.position.y, transform.position.z);
+
+        if (!calculated_new_pos)
+        {
+            new_position_behind_player = 0.5f + Random.Range(0.0f, 4.0f);
+            original_position_behind_player = position_behind_player;
+
+            calculated_new_pos = true;
+        }
+        else
+        {
+            current_lerp_time += Time.deltaTime;
+
+            float rate = (current_lerp_time / total_lerp_time);
+
+            position_behind_player = Mathf.Lerp(original_position_behind_player, new_position_behind_player, rate);
+
+            if (current_lerp_time > total_lerp_time)
+            {
+                position_behind_player = new_position_behind_player;
+                current_lerp_time = 0.0f;
+                calculated_new_pos = false;
+            }
+        }
+    }
+
+    void DestroyFigure()
+    {
+        player_controller.SubtractFigure();
     }
 
     void UpdateJumpingState()
@@ -110,6 +168,13 @@ public class FigureController : MonoBehaviour
 
     void InitVariables()
     {
+        rb  = GetComponent<Rigidbody2D>();
+        col = GetComponent<CapsuleCollider2D>();
 
+        GameObject player   = GameObject.FindGameObjectWithTag("Player");
+        player_transform    = player.transform;
+        player_controller   = player.GetComponent<PlayerController>();
+
+        position_behind_player = 0.5f + Random.Range(0.0f, 4.0f);
     }
 }
